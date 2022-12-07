4cee463317b2e883b00a8de8e5a97cf74756a1e8
Anders Larsson <andershaverdal@gmail.com>
2022-12-07 11:39:00 +0100

    Updated 2.7

---
+++ 2.7/assets/js/eb5f505b.209aa77d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknew_noodl_docs=self.webpackChunknew_noodl_docs||[]).push([[5977],{8897:function(n){n.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-blog","id":"whats-new"}}')}}]);
