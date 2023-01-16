5acb201c04a6881f7b5c99b7b4f0248a589d9c88
kotte <konstantin.aistre@gmail.com>
2023-01-16 13:13:44 +0100

    Feat: LAUNCH 2.8 DOCS!!

--- 2.8/assets/js/12d6026a.0c09faae.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknew_noodl_docs=self.webpackChunknew_noodl_docs||[]).push([[2901],{47754:function(e){e.exports=JSON.parse('{"permalink":"/2.8/whats-new/page/3","page":3,"postsPerPage":1,"totalPages":3,"totalCount":3,"previousPage":"/2.8/whats-new/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
