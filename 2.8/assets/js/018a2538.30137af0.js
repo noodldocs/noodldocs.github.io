c7e3ddcbcb89a3a13518c9101efe845354c00e6e
mikaeltellhed <mikael.tellhed@gmail.com>
2023-05-16 10:24:07 +0200

    Fixed ratings prefab and export PDF

--- 2.8/assets/js/018a2538.30137af0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknew_noodl_docs=self.webpackChunknew_noodl_docs||[]).push([[958],{83769:function(n){n.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
